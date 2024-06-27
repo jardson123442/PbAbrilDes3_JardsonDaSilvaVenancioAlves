669a66c574d4217c8fc6fad1a642c58b607bba7d
JardsonVenancio <126803792+jardson123442@users.noreply.github.com>
2024-06-27 10:54:59 -0300

    Creating - Microservices

---
+++ ms-costomer/src/main/java/br/com/jardson/mscostomer/MsCostomerApplication.java
@@ -0,0 +1,13 @@
+package br.com.jardson.mscostomer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCostomerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsCostomerApplication.class, args);
+	}
+
+}
